94593f3018223b5ddc105195652261a7fc021f16
Francisco Gilvando de Sousa Torres Júnior <juniorgilvando16@gmail.com>
2026-01-27 19:51:37 -0300

    Create Store class with methods and enum for options
    Added Store class with options for store operations and methods to manage store attributes.

--- src/utils/Store.java
+++ src/utils/Store.java
@@ -1,1 +1,40 @@
+public class Store {
 
+    public enum OpcoesLoja {
+        CADASTRAR(1);
+
+        private final int OPCAO;
+
+        OpcoesLoja(int opcao){
+            this.OPCAO = opcao;
+        }
+
+        public int getOption(){
+            return OPCAO;
+        }
+    }
+
+    private static String name;
+    private static int quantity;
+    private static double price;
+
+
+    public static String getName(){
+        return name;
+    }
+
+    public  String setName(String name){
+        this.name = name;
+        return name;
+    }
+
+    public double getPrice(){
+        return price;
+    }
+
+    public double setPrice(double price){
+        this.price = price;
+        return price;
+    }
+
+}
