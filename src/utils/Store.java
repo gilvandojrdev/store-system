e48fce0fd8c0098c5b0942ae4ed3fe77c4eef3c3
Francisco Gilvando de Sousa Torres Júnior <juniorgilvando16@gmail.com>
2026-02-02 18:37:49 -0300

    Change lista_produtos to List for better type safety
    Refactor lista_produtos to use List interface and improve type safety.

--- src/utils/Store.java
+++ src/utils/Store.java
@@ -1,14 +1,18 @@
 import java.io.Serializable;
 import java.util.ArrayList;
+import java.util.List;
 
 public class Products {
-
-    private ArrayList lista_produtos = new ArrayList<>();
+    private List<Object> lista_produtos = new ArrayList<>();
     private String divider = "-------------------";
 
     public Products(){
         this.lista_produtos = lista_produtos;
         this.divider = divider;
+    }
+
+    public List getLista(){
+        return lista_produtos;
     }
 
     public Serializable cadastrarProdutos(String produtos){
@@ -22,4 +26,17 @@ public class Products {
             System.out.println("["+i+"] " + lista_produtos.get(i));
         }
     }
-}
+
+    public String cadastrarPrecoProduto(String nome_produto, double preco_produto){
+        for (Object object : lista_produtos){
+
+            if (lista_produtos.contains(nome_produto)){
+                System.out.println("Produto de nome: " + nome_produto + " foi setado o preço " + preco_produto);
+            } else {
+                System.out.println("Esse produto não existe na lista");
+            }
+
+        }
+        return divider;
+    }
+};
