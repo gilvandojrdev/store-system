d58b4ec228900f66caddffe5d0e3fca6542359f7
Francisco Gilvando de Sousa Torres Júnior <juniorgilvando16@gmail.com>
2026-02-02 18:36:43 -0300

    Implement price setting for registered products
    Added functionality to set product prices after registration.

--- src/Application/Main.java
+++ src/Application/Main.java
@@ -9,6 +9,7 @@ public class Store {
         System.out.println("1 - Cadastrar um produto");
 
         String option = "s";
+        String option2 = "s";
 
         while (option.equals("s")) {
             System.out.println("Digite o nome do produto que deseja cadastrar: ");
@@ -19,9 +20,25 @@ public class Store {
 
             System.out.println("Deseja cadastrar mais produtos? (s ou n)");
             option = sc.next();
+
+            System.out.println("Deseja setar os preço dos produtos? (s ou n)");
+            option2 = sc.next();
         }
-        sc.close();
 
         prod.exibirProdutos();
+
+        while (option2.equals("s")){
+            System.out.println("Digite o nome do produto que você deseja setar o preço: ");
+            String nome_produto = sc.next();
+            System.out.println("Digite o preço do produto: " + nome_produto);
+            double preco_produto = sc.nextDouble();
+
+            prod.cadastrarPrecoProduto(nome_produto, preco_produto);
+
+            System.out.println("Deseja setar mais preços de produtos? (s ou n)");
+
+            option2 = sc.next();
+
+        }
     }
 }
