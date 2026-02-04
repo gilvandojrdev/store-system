bb260b7716c786d4ab5e1a75f4fb6c1450396374
Francisco Gilvando de Sousa Torres Júnior <juniorgilvando16@gmail.com>
2026-02-04 13:07:29 -0300

    Enhance Products class with price and name methods
    Added methods to get product name and price, and updated product registration.

--- src/utils/Store.java
+++ src/utils/Store.java
@@ -5,6 +5,8 @@ import java.util.List;
 public class Products {
     private List<Object> lista_produtos = new ArrayList<>();
     private String divider = "-------------------";
+    private double produto_preco;
+    private String nome_produto;
 
     public Products(){
         this.lista_produtos = lista_produtos;
@@ -14,6 +16,16 @@ public class Products {
     public List getLista(){
         return lista_produtos;
     }
+
+
+    public String getNomeProduto(){
+        return nome_produto;
+    }
+
+    public double getPrecoProduto(){
+        return produto_preco;
+    }
+
 
     public Serializable cadastrarProdutos(String produtos){
         lista_produtos.add(produtos);
@@ -28,10 +40,14 @@ public class Products {
     }
 
     public String cadastrarPrecoProduto(String nome_produto, double preco_produto){
+
+        this.produto_preco = preco_produto;
+        this.nome_produto = nome_produto;
+
         for (Object object : lista_produtos){
 
             if (lista_produtos.contains(nome_produto)){
-                System.out.println("Produto de nome: " + nome_produto + " foi setado o preço " + preco_produto);
+                System.out.println("Produto de nome: " + getNomeProduto() + " foi setado o preço " + getPrecoProduto());
             } else {
                 System.out.println("Esse produto não existe na lista");
             }
@@ -39,4 +55,9 @@ public class Products {
         }
         return divider;
     }
+
+    public String exibirDados(){
+        return "Informações da loja:\n produtos cadastrados:"  + lista_produtos + ", Produto cadastrados com valores:"; // Em desenvolvimento...
+    }
+
 };
