5acc931b2e6efef4aebbec5a23b5cb9aa2e2a2a6
Francisco Gilvando de Sousa Torres Júnior <juniorgilvando16@gmail.com>
2026-02-04 20:36:12 -0300

    Refactor product registration and pricing logic

--- src/Application/Main.java
+++ src/Application/Main.java
@@ -6,13 +6,10 @@ public class Store {
         Products prod = new Products();
 
         System.out.println("Boas-vindas a Store system");
-        System.out.println("1 - Cadastrar um produto");
-
         String option = "s";
         String option2 = "s";
 
-
-        // Tenho que refatorar a lógica de adicionar mais produtos...
+        // Logica quase pronta tenho que otimizar a o cadastro de produtos tornando em função para chamar ela novamente sem fazer código denovo.
         while (option.equals("s")) {
             System.out.println("Digite o nome do produto que deseja cadastrar: ");
             String nomeProduto = sc.next();
@@ -20,32 +17,45 @@ public class Store {
             System.out.println("Produto " + nomeProduto + " cadastrado com sucesso");
             System.out.println(cadastrar_produtos);
 
-            System.out.println("Deseja cadastrar mais produtos? (s ou n)");
+            System.out.println("Deseja cadastrar mais produtos? (s ou n) | digite uma opção abaixo");
             option = sc.next();
 
             if(option.equals("n")){
-                System.out.println("Deseja setar os preço dos produtos? (s ou n)");
+                System.out.println("Deseja setar os preço dos produtos? (s ou n) | digite uma opção abaixo");
+                option2 = sc.next();
+
+                if (option2.equals("s")){
+                    System.out.println("Digite o nome do produto que você deseja setar o preço: ");
+                    String nome_produto = sc.next();
+                    System.out.println("Digite o preço do produto: " + nome_produto);
+                    double preco_produto = sc.nextDouble();
+
+                    System.out.println(prod.cadastrarPrecoProduto(nome_produto, preco_produto));
+
+                    System.out.println("Deseja setar mais preços de produtos? (s ou n) | digite uma opção abaixo");
+                    option2 = sc.next();
+
+                    if (option2.equals("n")){
+                        System.out.println("Etapas concluidas deseja ver as informações de produtos? (s ou n) \n digite uma opção abaixo ");
+                        option2 = sc.next();
+                        if(option2.equals("s")){
+                            prod.exibirDados();
+                        } else if (option2.equals("n")){
+                            System.out.println("Finalizado");
+                            break;
+                        }
+                        else {
+                            System.out.println("Digite apenas (s ou n) | digite uma opção abaixo");
+                            option2 = sc.next();
+                        }
+                    }
+                }
+            } else if (option.equals("s")) {
+
+            } else {
+                System.out.println("Digite apenas (s ou n) | digite uma opção abaixo");
                 option2 = sc.next();
             }
         }
-
-        prod.exibirProdutos();
-
-        while (option2.equals("s")){
-            System.out.println("Digite o nome do produto que você deseja setar o preço: ");
-            String nome_produto = sc.next();
-            System.out.println("Digite o preço do produto: " + nome_produto);
-            double preco_produto = sc.nextDouble();
-
-            prod.cadastrarPrecoProduto(nome_produto, preco_produto);
-
-            System.out.println("Deseja setar mais preços de produtos? (s ou n)");
-
-            option2 = sc.next();
-
-        }
-
-        System.out.println(prod.exibirDados());
-
     }
 }
