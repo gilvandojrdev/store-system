c2a73ffa47e73620e1b0532a6efc1115d921d135
Francisco Gilvando de Sousa Torres Júnior <juniorgilvando16@gmail.com>
2026-02-07 15:06:07 -0300

    Refactor product registration and price setting flow

--- src/Application/Main.java
+++ src/Application/Main.java
@@ -6,42 +6,49 @@ public class Store {
         Products prod = new Products();
         System.out.println("Boas vindas a loja");
         String input = "s";
+        String input2 = "s";
 
         while (input.equals("s")){
             prod.cadastrarProdutos();
+            prod.exibirProdutos();
+
             System.out.println("Deseja adicionar mais? digite (s ou n)");
             input = sc.next();
 
-            if (input.equals("s")){
-                prod.cadastrarProdutos();
-                System.out.println("Deseja adicionar mais? digite (s ou n)");
-                input = sc.next();
-            } else {
-                System.out.println("digite apenas (s ou n)");
+            while (input.equals("s")){
+                if (input.equals("s")){
+                    prod.cadastrarProdutos();
+                    prod.exibirProdutos();
+                    System.out.println("Deseja adicionar mais? digite (s ou n)");
+                    input = sc.next();
+                } else {
+                    System.out.println("digite apenas (s ou n)");
+                }
             }
 
-            if (input.equals("n")){
+            while (input.equals("n")){
                 System.out.println("Deseja setar os preços dos produtos cadastrados digite (s ou n)");
                 input = sc.next();
 
                 if (input.equals("s")){
-                    prod.cadastrarPrecoProduto();
-                    System.out.println("Deseja setar mais preços dos produtos cadastrados digite (s ou n)");
-                    input = sc.next();
-                }
-                
-                if(input.equals("n")){
-                    System.out.println("Deseja ver os produtos cadastrados? digite (s ou n)");
-                    input = sc.next();
-
-                    if (input.equals("s")){
-                        System.out.println(prod.exibirDados());
-                        break;
+                    while (input.equals("s")){
+                        prod.cadastrarPrecoProduto();
+                        System.out.println("Deseja setar mais preços dos produtos cadastrados digite (s ou n)");
+                        input = sc.next();
                     }
-
                     if (input.equals("n")){
-                        System.out.println("finalizado");
-                        break;
+                        System.out.println("Deseja ver os produtos cadastrados? digite (s ou n)");
+                        input2 = sc.next();
+
+                        if (input2.equals("s")){
+                            System.out.println(prod.exibirDados()); 
+                            break;
+                        }
+
+                        if (input2.equals("n")){
+                            System.out.println("finalizado");
+                            break;
+                        }
                     }
                 }
             }
