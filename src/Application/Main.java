b402b9e80d6d6c0974458186855a9a70ce294cd2
Francisco Gilvando de Sousa Torres Júnior <juniorgilvando16@gmail.com>
2026-02-08 12:45:26 -0300

    Refactor main method and simplify product input flow

--- src/Application/Main.java
+++ src/Application/Main.java
@@ -1,57 +1,41 @@
+package dev.frgilvando;
+
+import dev.frgilvando.utils.Products;
+import java.util.ArrayList;
+import java.util.List;
+import java.text.NumberFormat;
+import java.util.Locale;
 import java.util.Scanner;
 
 public class Store {
-    static void main(String[] args) {
+    public static void main(String[] args) {
+        List<Products> listaProdutos = new ArrayList<>();
         Scanner sc = new Scanner(System.in);
-        Products prod = new Products();
+        NumberFormat formatador = NumberFormat.getCurrencyInstance(new Locale("pt", "BR"));
+
         System.out.println("Boas vindas a loja");
-        String input = "s";
-        String input2 = "s";
+        String continuar = "s";
 
-        while (input.equals("s")){
-            prod.cadastrarProdutos();
-            prod.exibirProdutos();
+        while (continuar.equalsIgnoreCase("s")){
+            System.out.print("Digite o nome do produto: ");
+            String nome = sc.next();
 
-            System.out.println("Deseja adicionar mais? digite (s ou n)");
-            input = sc.next();
+            System.out.print("Digite o preço do produto: ");
+            double preco = sc.nextDouble();
+            sc.nextLine();
 
-            while (input.equals("s")){
-                if (input.equals("s")){
-                    prod.cadastrarProdutos();
-                    prod.exibirProdutos();
-                    System.out.println("Deseja adicionar mais? digite (s ou n)");
-                    input = sc.next();
-                } else {
-                    System.out.println("digite apenas (s ou n)");
-                }
+            listaProdutos.add(new Products(nome, preco));
+
+            System.out.println("\n--- Lista de Produtos ---");
+            for (Products p : listaProdutos) {
+                System.out.println("Produto: " + p.getNome() + " | Preço: " + formatador.format(p.getPreco()));
             }
 
-            while (input.equals("n")){
-                System.out.println("Deseja setar os preços dos produtos cadastrados digite (s ou n)");
-                input = sc.next();
-
-                if (input.equals("s")){
-                    while (input.equals("s")){
-                        prod.cadastrarPrecoProduto();
-                        System.out.println("Deseja setar mais preços dos produtos cadastrados digite (s ou n)");
-                        input = sc.next();
-                    }
-                    if (input.equals("n")){
-                        System.out.println("Deseja ver os produtos cadastrados? digite (s ou n)");
-                        input2 = sc.next();
-
-                        if (input2.equals("s")){
-                            System.out.println(prod.exibirDados()); 
-                            break;
-                        }
-
-                        if (input2.equals("n")){
-                            System.out.println("finalizado");
-                            break;
-                        }
-                    }
-                }
-            }
+            System.out.print("\nDeseja adicionar outro? (s/n): ");
+            continuar = sc.next();
         }
+
+        sc.close();
+        System.out.println("Sistema encerrado.");
     }
 }
